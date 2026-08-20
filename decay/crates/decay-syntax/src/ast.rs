b966651c0546253131f047b36ba08ed44152792e
Christoffer Madsen <126353638+MadsenDev@users.noreply.github.com>
2026-08-20 22:54:59 +0200

    Add Decay syntax AST

---
+++ decay/crates/decay-syntax/src/ast.rs
@@ -0,0 +1,164 @@
+use crate::Span;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Program {
+    pub items: Vec<Item>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Item {
+    Script(ContainerDecl),
+    Component(ContainerDecl),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ContainerDecl {
+    pub name: String,
+    pub members: Vec<Member>,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Member {
+    Field(FieldDecl),
+    Function(FunctionDecl),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Attribute {
+    pub name: String,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct FieldDecl {
+    pub attributes: Vec<Attribute>,
+    pub mutable: bool,
+    pub name: String,
+    pub ty: Option<TypeRef>,
+    pub initializer: Option<Expr>,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct TypeRef {
+    pub name: String,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct FunctionDecl {
+    pub name: String,
+    pub params: Vec<Param>,
+    pub return_type: Option<TypeRef>,
+    pub body: Block,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Param {
+    pub name: String,
+    pub ty: Option<TypeRef>,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Block {
+    pub statements: Vec<Stmt>,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Stmt {
+    Binding {
+        mutable: bool,
+        name: String,
+        ty: Option<TypeRef>,
+        initializer: Option<Expr>,
+        span: Span,
+    },
+    Expr {
+        expr: Expr,
+        span: Span,
+    },
+    Return {
+        value: Option<Expr>,
+        span: Span,
+    },
+    If {
+        condition: Expr,
+        then_branch: Block,
+        else_branch: Option<Block>,
+        span: Span,
+    },
+    Block(Block),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Expr {
+    pub kind: ExprKind,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ExprKind {
+    Identifier(String),
+    Number(f64),
+    String(String),
+    Bool(bool),
+    Null,
+    Unary {
+        op: UnaryOp,
+        expr: Box<Expr>,
+    },
+    Binary {
+        left: Box<Expr>,
+        op: BinaryOp,
+        right: Box<Expr>,
+    },
+    Assign {
+        target: Box<Expr>,
+        op: AssignOp,
+        value: Box<Expr>,
+    },
+    Member {
+        object: Box<Expr>,
+        field: String,
+    },
+    Call {
+        callee: Box<Expr>,
+        args: Vec<Expr>,
+    },
+    Group(Box<Expr>),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum UnaryOp {
+    Negate,
+    Not,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum BinaryOp {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Equal,
+    NotEqual,
+    Less,
+    LessEqual,
+    Greater,
+    GreaterEqual,
+    And,
+    Or,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum AssignOp {
+    Assign,
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+}
