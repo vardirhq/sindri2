0dd7575d81eef2238419aca2a48891e087e85a26
Christoffer Madsen <126353638+MadsenDev@users.noreply.github.com>
2026-08-20 23:22:17 +0200

    chore(decay): remove generated build artifacts

--- decay/target/debug/deps/decay_runtime-492e04c374eb24f5.d
+++
@@ -1,9 +0,0 @@
-/home/runner/work/sindri2/sindri2/decay/target/debug/deps/decay_runtime-492e04c374eb24f5.d: crates/decay-runtime/src/lib.rs Cargo.toml
-
-/home/runner/work/sindri2/sindri2/decay/target/debug/deps/libdecay_runtime-492e04c374eb24f5.rmeta: crates/decay-runtime/src/lib.rs Cargo.toml
-
-crates/decay-runtime/src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__--no-deps__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
