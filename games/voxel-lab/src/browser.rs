94494c5382e53def35b1b9e88087b664641d001c
Christoffer Madsen <126353638+MadsenDev@users.noreply.github.com>
2026-09-20 23:08:51 +0200

    Make Voxel Lab atlas setup rustfmt-unambiguous

--- games/voxel-lab/src/browser.rs
+++ games/voxel-lab/src/browser.rs
@@ -75,20 +75,19 @@ impl DesktopApp for VoxelLabApp {
 
     fn create(context: &AppContext<'_>) -> Result<Self, Self::Error> {
         let mut textures = TextureRegistry::new(context.device(), context.queue());
-        let material_textures = [
-            causeway_atlas(
-                context,
-                &mut textures,
-                "textures/blocks-top.png",
-                CAUSEWAY_TOPS,
-            )?,
-            causeway_atlas(
-                context,
-                &mut textures,
-                "textures/blocks-side.png",
-                CAUSEWAY_SIDES,
-            )?,
-        ];
+        let top_texture = causeway_atlas(
+            context,
+            &mut textures,
+            "textures/blocks-top.png",
+            CAUSEWAY_TOPS,
+        )?;
+        let side_texture = causeway_atlas(
+            context,
+            &mut textures,
+            "textures/blocks-side.png",
+            CAUSEWAY_SIDES,
+        )?;
+        let material_textures = [top_texture, side_texture];
         Ok(Self {
             lab: VoxelLabRuntime::new(),
             textures,
