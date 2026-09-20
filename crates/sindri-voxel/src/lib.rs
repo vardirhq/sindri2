f1eeb7b7c37442a651193e17f28d996669c29bb4
Christoffer Madsen <126353638+MadsenDev@users.noreply.github.com>
2026-09-20 15:38:18 +0200

    Format voxel foundation

--- crates/sindri-voxel/src/lib.rs
+++ crates/sindri-voxel/src/lib.rs
@@ -7,6 +7,6 @@ mod coord;
 mod section;
 mod source;
 
-pub use coord::{LocalVoxelCoord, SectionCoord, VoxelCoord, SECTION_EDGE, SECTION_VOLUME};
+pub use coord::{LocalVoxelCoord, SECTION_EDGE, SECTION_VOLUME, SectionCoord, VoxelCoord};
 pub use section::{VoxelId, VoxelSection};
 pub use source::VoxelSource;
