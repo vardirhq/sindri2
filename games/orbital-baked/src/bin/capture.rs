1a7b1c9b5f15383fd02c1085db24e4480984e713
Christoffer Madsen <126353638+MadsenDev@users.noreply.github.com>
2026-09-22 22:44:40 +0200

    Apply rustfmt output

--- games/orbital-baked/src/bin/capture.rs
+++ games/orbital-baked/src/bin/capture.rs
@@ -18,9 +18,9 @@ use sindri_assets::{AssetBytes, AssetDecoder, FontAssetDecoder, TextureAssetDeco
 use sindri_core::{AssetId, SpriteSheetDocument, sheet_id_for};
 use sindri_gpu::{GpuContext, GpuRequestOptions};
 use sindri_render::{
-    Bloom, BloomSettings, PostProcessSettings, DepthTarget, FrameRenderers, FrameTarget, GlyphRenderer, Lighting,
-    OffscreenTarget, ShapeRenderer, SpriteBatchRenderer, TextRenderer, Texture2D, TextureRegistry,
-    TexturedCubeRenderer, Viewport, encode_lit_frame,
+    Bloom, BloomSettings, DepthTarget, FrameRenderers, FrameTarget, GlyphRenderer, Lighting,
+    OffscreenTarget, PostProcessSettings, ShapeRenderer, SpriteBatchRenderer, TextRenderer,
+    Texture2D, TextureRegistry, TexturedCubeRenderer, Viewport, encode_lit_frame,
 };
 use sindri_scene::{CameraView, SceneRuntime, TextureBindings, UiCanvas, WorldProjection};
 
